b52f5183ed2642ded5cb3c562e376d09badab7d1
Andrii_Petrukh <andriypetruch2004@gmail.com>
2025-09-07 23:19:19 +0300

    done form

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const form = document.querySelector(".feedback-form")
+
+let formData = {
+    email: "",
+    message: "" 
+}
+
+const STORAGE_KEY = "feedback-form-state"
+
+const saveData = localStorage.getItem(STORAGE_KEY)
+if(saveData){
+    formData.JSON.parse(saveData)
+    form.elements.email.value = formData.email || ""
+    form.elements.message.value = formData.message || ""
+}
+
+form.addEventListener("input" , (e) => {
+    formData[e.target.name] = e.target.value.trim()
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    if(!formData.email || !formData.message){
+        alert("Fill please all fields")
+        return
+    }
+    console.log("Submitted data:", formData);
+    
+
+    formData = {email: '', message: ''}
+    localStorage.removeItem(STORAGE_KEY)
+    form.reset()
+})
+
+
